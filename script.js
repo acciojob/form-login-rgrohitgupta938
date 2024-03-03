91a8cb4e2c9bb06d780a14ab8befd6960b5a2ea6
rgrohitgupta938 <49370602+rgrohitgupta938@users.noreply.github.com>
2024-03-03 15:59:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+	const fName = document.getElementsByName("fname")[0].value;
+    const lName = document.getElementsByName("lname")[0].value;
+	alert(`${fName} ${lName}`);
 }
